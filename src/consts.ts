5f5c445ce62315359b8cbd2032a918dd837c2c70
Michael Simon AZ <michaelsimonaz@gmail.com>
2023-04-13 13:20:14 -0700

    added tailwind

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Arizona Monster";
+export const SITE_DESCRIPTION =
+  "Arizona Monster is a blog about the Arizona Monster, a mysterious creature that was sighted in the 1950s in Arizona.";
